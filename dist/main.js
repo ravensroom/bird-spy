9823c298ba8adab2d5eba9f2f8399d1d425c1b5c
raven <raven.lin.010@gmail.com>
2023-06-18 20:04:04 -0700

    Migrate to typescript and esm

---
+++ dist/main.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+import { default as config } from './config.json' assert { type: 'json' };
+const { jobSites, listOfSearchKeywords, maxEntriesPerQuery, location, priority, } = config;
+const timeRange = config.timeRange;
+const titleShouldExclude = new Set(config.titleShouldExclude);
+const titleShouldInclude = new Set(config.titleShouldInclude);
+const { linkedIn } = jobSites;
+async function getLinkedInPosts() {
+    const jobs = new Map();
+    for (let keywords of listOfSearchKeywords) {
+        let start = 0, totalEntries;
+        do {
+            const url = linkedIn.url.base +
+                linkedIn.url.timeRange[timeRange] +
+                '&' +
+                linkedIn.url.keywords +
+                keywords.split(' ').join('%20') +
+                '&' +
+                linkedIn.url.location +
+                location.split(' ').join('%20') +
+                '&' +
+                linkedIn.url.pagination +
+                start;
+            const $ = await getParsedHTML(url);
+            if (!totalEntries)
+                totalEntries = +$('.results-context-header__job-count')
+                    .text()
+                    .replace(/[\,\+]/g, '');
+            const jobCards = $('.jobs-search__results-list .base-card');
+            jobCards.each((index, jobCard) => {
+                const jobId = $(jobCard)?.attr('data-entity-urn')?.split(':').at(-1);
+                if (!jobId)
+                    return;
+                if (jobs.has(jobId))
+                    return;
+                const linkElement = $(jobCard).find('.base-card__full-link')[0];
+                const href = $(linkElement).attr('href');
+                const cardInfo = $(jobCard).find('.base-search-card__info').eq(0);
+                const title = $(cardInfo)
+                    .find('.base-search-card__title')
+                    .eq(0)
+                    .text()
+                    .trim();
+                const company = $(cardInfo)
+                    .find('.base-search-card__subtitle a')
+                    .eq(0)
+                    .text()
+                    .trim();
+                const location = $(cardInfo)
+                    .find('.job-search-card__location')
+                    .eq(0)
+                    .text()
+                    .trim();
+                const titleWords = title.split(/\s+|[,;|]/);
+                let isDesired = false;
+                for (let word of titleWords) {
+                    if (titleShouldExclude.has(word.toLowerCase()))
+                        return;
+                    if (titleShouldInclude.has(word.toLowerCase()))
+                        isDesired = true;
+                }
+                if (isDesired) {
+                    jobs.set(jobId, {
+                        id: jobId,
+                        title,
+                        company,
+                        href,
+                        location,
+                    });
+                }
+            });
+            start += linkedIn.maxEntriesPerPage;
+        } while (start <
+            (maxEntriesPerQuery
+                ? Math.min(maxEntriesPerQuery, totalEntries)
+                : totalEntries));
+    }
+    return jobs;
+}
+async function getParsedHTML(url) {
+    const response = await axios.get(url);
+    const html = response.data;
+    return cheerio.load(html);
+}
+async function spy() {
+    try {
+        const linkedInJobs = await getLinkedInPosts();
+        console.log(linkedInJobs);
+    }
+    catch (error) {
+        console.error('Error:', error);
+    }
+}
+spy();
