d20cbedc41cef479392b08401c05626a7c712559
raven <raven.lin.010@gmail.com>
2023-06-21 02:41:40 -0700

    ui: fix bird head

--- ui/src/components/jobList/JobItem.tsx
+++ ui/src/components/jobList/JobItem.tsx
@@ -14,48 +14,52 @@ const JobItem: React.FC<JobItemProps> = ({ job }) => {
   };
 
   return (
-    <div className="flex flex-col border-t-8 border-blue-800 shadow ">
-      <div className="flex justify-between bg-indigo-300 rounded-md rounded-t-none">
+    <div className="flex flex-col border-t-[1px] border-indigo-200 shadow-inner ">
+      <div className="flex justify-between items-center bg-indigo-300 rounded-md rounded-t-none">
         <div className="flex flex-col ">
-          <div className="font-bold text-gray-800 text-lg px-2 pt-1">
+          <div className="font-bold text-gray-800 ml-2">
             <a
               href={href}
               target="_blank"
-              className="hover:bg-indigo-400 active:bg-indigo-500 px-2"
+              className="text-sm hover:bg-indigo-400 active:bg-indigo-500"
             >
               {title}
             </a>
           </div>
-
-          <div className="text-xs mb-2 text-gray-800">
-            <span className="mx-4">Company: {company}</span>
-            <span>Location: {location}</span>
+          <div className="text-[12px] mb-2 ml-2 ">
+            <span className="text-gray-800">{company}</span>
+            <span className="pl-2 text-gray-600">{location}</span>
           </div>
         </div>
 
         <div
           onClick={handleClickBird}
-          className="mx-2 mt-[10px] mb-2 shadow-inner shadow-black font-extrabold rounded-full text-[7px] pt-4 flex flex-col items-center hover:cursor-pointer bg-indigo-400 hover:bg-indigo-500"
+          className="w-8 h-8 pt-2 mr-3 text-[8px]   shadow-inner shadow-black font-extrabold rounded-full flex flex-col items-center hover:cursor-pointer bg-indigo-400 hover:bg-indigo-500"
         >
-          <span
+          <div
             className={`${
               isOpen ? 'bg-indigo-300' : 'bg-gray-900'
-            } px-2 shadow-inner shadow-blac `}
+            } px-[6px] pt-[2px] shadow-inner shadow-black flex gap-2`}
           >
-            O<span className="px-[5px]"></span> =
-          </span>
-          <span className="text-[8px]">
+            <span>O</span>
+            <span>=</span>
+          </div>
+
+          <div className="text-[8px]">
             <span className={isOpen ? 'hidden' : 'inline'}>V</span>
-            <span className={isOpen ? 'inline' : 'hidden'}>o</span>
-          </span>
+            <span className={isOpen ? 'inline' : 'hidden'}>O</span>
+          </div>
         </div>
       </div>
       <div
-        className={`px-5 py-2 shadow-md shadow-blue-800 text-md bg-indigo-200 mx-4 mb-2 sm:mx-6 text-slate-900 ${
+        className={`px-5 py-2 shadow-md shadow-blue-800 text-sm bg-indigo-200 mx-4 mb-2 sm:mx-6 text-slate-900 ${
           isOpen ? 'block' : 'hidden'
         }`}
       >
-        <div dangerouslySetInnerHTML={{ __html: description }}></div>
+        <div
+          className="text-sm"
+          dangerouslySetInnerHTML={{ __html: description }}
+        ></div>
       </div>
     </div>
   );
