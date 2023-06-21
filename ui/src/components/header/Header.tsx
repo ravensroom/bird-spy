40add39a20649dc1d03d59fa2e61d096cebda184
raven <raven.lin.010@gmail.com>
2023-06-21 02:46:50 -0700

    ui: scroll to top

--- ui/src/components/header/Header.tsx
+++ ui/src/components/header/Header.tsx
@@ -1,7 +1,14 @@
 const Header = () => {
+  const handleHeaderClick = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <div className="top-0 w-full flex justify-center">
-      <div className="fixed bg-indigo-500 text-gray-100 text-base px-5 font-bold font-mono shadow-md shadow-slate-600">
+      <div
+        onClick={handleHeaderClick}
+        className="hover:cursor-pointer fixed bg-indigo-500 text-gray-100 text-base px-5 font-bold font-mono shadow-md shadow-slate-600"
+      >
         Bird Spying...
       </div>
     </div>
